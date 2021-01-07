c15c1cdd9930fc52a82cc347dc023e74bbc11c53
WisdomerLee <cheonnyeo@gmail.com>
2021-01-08 08:58:08 +0900

    ....
    네트워크 부분 추가 시작할 예정

---
+++ Network/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+//참고 사이트: https://nowonbun.tistory.com/246
+//비동기 소켓 방식 중 이벤트 기반 비동기식 소켓 접속 방식
+
+
+namespace Network
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
